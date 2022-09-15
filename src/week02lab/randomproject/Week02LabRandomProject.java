e304c9b2eb831ced3744998553cb25ae8ebebed0
roker <roker@DESKTOP-TJV53Q5.lan>
2022-09-15 08:59:08 -0600

    New message added Created a new message upon running the application

--- src/week02lab/randomproject/Week02LabRandomProject.java
+++ src/week02lab/randomproject/Week02LabRandomProject.java
@@ -16,7 +16,7 @@ public class Week02LabRandomProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.print("Hello world!");
     }
     
 }
